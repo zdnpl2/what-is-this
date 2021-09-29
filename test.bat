1dcbde712a61f71e3b3e3436e04e9fd4d232309d
zdnpl2 <90134548+zdnpl2@users.noreply.github.com>
2021-09-29 07:40:58 +0700

    1st commit

---
+++ test.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Scanning virus and malware
+pause
+dir/s
+echo Scanning completed
+pause
+echo 2 files has been blocked
+pause
+echo deleting 2 files
+echo restarting system
+shutdown -r -t 0
